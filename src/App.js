91d96ddac98a3690f96fde7116b6fde1fdbc7acb
Chetan Dhangar <chetansdhangar@gmail.com>
2020-12-07 18:47:10 +0530

    Functinality done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
+const musicTrivia ={
+  "Happy" : [
+    {
+      "songName" : "Tik Tik Vajtey Dokyat",
+      "rating" : '4.5/5',
+    
+    },
+    {
+      "songName" : "Gulabachi Kali",
+      "rating" : "3.5/5"
+    }
+  ],
+  
+  "Sad" : [
+    {
+      "songName" : "Deval Kalji re",
+      "rating" : "5/5",
+    }
+  ],
+
+}
+
+var musicList = Object.keys(musicTrivia);
+
+
+
+
 function App() {
+
+  const [song , setSong] = useState("Sad");
+
+  function clickHandler(music){
+    setSong(music)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FeelTheRealMusic</h1>
+
+      <div className="content">
+        {musicList.map(music=>{
+          return <span style={{padding:"2rem", cursor:"pointer"}}
+          onClick={()=>clickHandler(music)}
+          >{music}</span>
+        })}
+
+        <h1>display</h1>
+
+        <div>
+          {musicTrivia[song].map((song)=>{
+            return(
+              <ul key={song.songName}>
+                <li>
+                  <div>{song.songName}</div>
+                  <div>{song.rating}</div>
+                </li>
+              </ul>
+            );
+          })}
+        </div>
+
+      </div>
     </div>
   );
 }
